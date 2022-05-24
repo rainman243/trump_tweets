ead3c30ae1119f9b0bafc31b9684b2a980615d77
Matthew <mattwoods243@outlook.com>
2022-05-23 23:29:43 -1000

    add multiple input text boxes for labels

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -4,29 +4,29 @@ from bs4 import BeautifulSoup
 import sys
 from pprint import pprint as p
 
-with open("more_features.json") as f:
-    dict_features = json.load(f)
-p(dict_features)
-
 class TweetDataEntryTool(QWidget):
         def __init__(self,parent=None):
                 super().__init__(parent)
 
+                with open("more_features.json") as f:
+                    self.dict_features = json.load(f)
+                    
                 self.setWindowTitle("Trump Tweet Data Entry Tool")
                 self.resize(1344, 756)
-
-                self.textEdit = QTextEdit()
+                
                 self.btnPress1 = QPushButton("Forward")
                 self.btnPress2 = QPushButton("Back")
-
+                self.btnPress1.clicked.connect(self.btnPress1_Clicked)
+                self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+                
                 layout = QVBoxLayout()
-                layout.addWidget(self.textEdit)
                 layout.addWidget(self.btnPress1)
                 layout.addWidget(self.btnPress2)
                 self.setLayout(layout)
-
-                self.btnPress1.clicked.connect(self.btnPress1_Clicked)
-                self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+                
+                for i in range(20):
+                    exec("self.textEdit{i} = QTextEdit()".format(i=i))
+                    exec("layout.addWidget(self.textEdit{i})".format(i=i))
 
         def btnPress1_Clicked(self):
                 self.textEdit.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
