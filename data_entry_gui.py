eb4f09262613dee8b963bcd753c418ca0a3a59ec
Matthew <mattwoods243@outlook.com>
2022-05-24 00:34:49 -1000

    add comments

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -8,19 +8,24 @@ class TweetDataEntryTool(QWidget):
         
         def __init__(self,parent=None):
                 super().__init__(parent)
-
+                
+                # Load labels
                 with open("more_features.json") as f:
                     self.dict_features = json.load(f)
                 
+                # Load feature whitelist
                 with open("feature_whitelist.json") as f:
                     self.feature_whitelist = list(json.load(f))
                 
+                # Load tweets
                 with open("high_rt_tweets.json") as f:
                     self.tweets = json.load(f)
-                    
+                
+                # Set window
                 self.setWindowTitle("Trump Tweet Data Entry Tool")
                 self.resize(1344, 756)
                 
+                # Add buttons
                 self.btnPress1 = QPushButton("Forward")
                 self.btnPress2 = QPushButton("Back")
                 self.btnPress3 = QPushButton("Exit")
@@ -28,23 +33,26 @@ class TweetDataEntryTool(QWidget):
                 self.btnPress1.clicked.connect(self.btnPress1_Clicked)
                 self.btnPress2.clicked.connect(self.btnPress2_Clicked)
                 
+                # Add layouts
                 layout_main = QHBoxLayout()
                 layout_left = QVBoxLayout()
                 layout_right = QVBoxLayout()
-                
                 layout_main.addLayout(layout_left)
                 layout_main.addLayout(layout_right)
                     
+                # Add elements
                 layout_right.addWidget(self.btnPress1)
                 layout_right.addWidget(self.btnPress2)
                 layout_right.addWidget(self.btnPress3)
                 layout_right.addWidget(QTextEdit())
                 layout_right.addWidget(QTextEdit())
                 
+                # Select current tweet
                 self.this_tweet = QLabel()
                 self.this_tweet.setText(self.tweets["0"])
                 layout_left.addWidget(self.this_tweet)
-                    
+                
+                # Add feature entry windows
                 for i in range(10):
                     exec("self.textEdit{i} = QTextEdit()".format(i=i))
                     exec("layout_left.addWidget(self.textEdit{i})".format(i=i))
