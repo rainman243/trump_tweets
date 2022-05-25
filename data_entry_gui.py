077e3d34b6a46e86ef7312e227a3172d94713c3b
Matthew <mattwoods243@outlook.com>
2022-05-24 22:30:10 -1000

    change variable names to be more descriptive

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -1,3 +1,5 @@
+# Data Entry GUI for more_feature.json
+
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox
 from PyQt5.QtGui import QFont
 import json
@@ -30,16 +32,16 @@ class TweetDataEntryTool(QWidget):
                 self.resize(1008, 756)
                 
                 # Add buttons
-                self.btnPress1 = QPushButton("Forward")
-                self.btnPress1.setFont(this_font)
-                self.btnPress2 = QPushButton("Back")
-                self.btnPress2.setFont(this_font)
-                self.btnPress3 = QPushButton("Exit")
-                self.btnPress3.setFont(this_font)
+                self.btnPressFwd = QPushButton("Forward")
+                self.btnPressFwd.setFont(this_font)
+                self.btnPressBack = QPushButton("Back")
+                self.btnPressBack.setFont(this_font)
+                self.btnPressExit = QPushButton("Exit")
+                self.btnPressExit.setFont(this_font)
 
-                self.btnPress1.clicked.connect(self.btnPress1_Clicked)
-                self.btnPress2.clicked.connect(self.btnPress2_Clicked)
-                self.btnPress3.clicked.connect(self.btnPress3_Clicked)
+                self.btnPressFwd.clicked.connect(self.btnPressFwd_Clicked)
+                self.btnPressBack.clicked.connect(self.btnPressBack_Clicked)
+                self.btnPressExit.clicked.connect(self.btnPressExit_Clicked)
                 
                 # Add layouts
                 layout_main = QHBoxLayout()
@@ -49,9 +51,9 @@ class TweetDataEntryTool(QWidget):
                 layout_main.addLayout(layout_right)
                 
                 # Add elements
-                layout_right.addWidget(self.btnPress1)
-                layout_right.addWidget(self.btnPress2)
-                layout_right.addWidget(self.btnPress3)
+                layout_right.addWidget(self.btnPressFwd)
+                layout_right.addWidget(self.btnPressBack)
+                layout_right.addWidget(self.btnPressExit)
                 
                 # Select current tweet
                 self.this_tweet = QLabel()
@@ -65,25 +67,25 @@ class TweetDataEntryTool(QWidget):
                 self.this_key = list(self.dict_features.keys())[0]
                 
                 # Add tweet number from top RT tweets (0 = highest RTs)
-                tweet_num = QLabel()
-                tweet_num.setText("Tweet number from top:")
-                tweet_num.setFont(this_font)
-                layout_right.addWidget(tweet_num)
-                tweet_num_enter = QTextEdit()
-                tweet_num_enter.setText("0")
-                layout_right.addWidget(tweet_num_enter)
+                self.tweet_num = QLabel()
+                self.tweet_num.setText("Tweet number from top:")
+                self.tweet_num.setFont(this_font)
+                layout_right.addWidget(self.tweet_num)
+                self.tweet_num_enter = QTextEdit()
+                self.tweet_num_enter.setText("0")
+                layout_right.addWidget(self.tweet_num_enter)
                 
                 # Add tweet ID
-                tweet_id = QLabel()
-                tweet_id.setText("Tweet ID:")
-                tweet_id.setFont(this_font)
-                layout_right.addWidget(tweet_id)
-                enter_id = QTextEdit()
-                enter_id.setText(self.this_id)
-                layout_right.addWidget(enter_id)
+                self.tweet_id = QLabel()
+                self.tweet_id.setText("Tweet ID:")
+                self.tweet_id.setFont(this_font)
+                layout_right.addWidget(self.tweet_id)
+                self.enter_id = QTextEdit()
+                self.enter_id.setText(self.this_id)
+                layout_right.addWidget(self.enter_id)
                 
                 # Add feature entry windows
-                for i in range(10):
+                for i in range(9):
                     exec("self.textEdit{i} = QTextEdit()".format(i=i))
                     exec("layout_left.addWidget(self.textEdit{i})".format(i=i))
                     exec("self.textEdit{i}.resize(100, 250)".format(i=i))
@@ -93,16 +95,13 @@ class TweetDataEntryTool(QWidget):
                         
                 self.setLayout(layout_main)
 
-        def extract_Id(self):
-            return 
-
-        def btnPress1_Clicked(self):
+        def btnPressFwd_Clicked(self):
                 self.textEdit.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
 
-        def btnPress2_Clicked(self):
+        def btnPressBack_Clicked(self):
                 self.textEdit.setHtml("<font color='red' size='6'><red>Hello PyQt5!\nHello</font>")
                 
-        def btnPress3_Clicked(self):
+        def btnPressExit_Clicked(self):
                 self.textEdit.setHtml("!")
 
 if __name__ == '__main__':
