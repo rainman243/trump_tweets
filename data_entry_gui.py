28ff444aa0aae010ee27ccb69bbe7f6058e978da
Matthew <mattwoods243@outlook.com>
2022-05-30 20:25:31 -1000

    Most GUI callbacks complete. TODO: create callback for saving features

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -7,6 +7,9 @@ from bs4 import BeautifulSoup
 import sys
 import re
 from pprint import pprint as p
+import inspect
+
+NUM_WINDOWS = 9
 
 class TweetDataEntryTool(QWidget):
         
@@ -19,7 +22,6 @@ class TweetDataEntryTool(QWidget):
                 with open("more_features.json") as f:
                     self.dict_features = json.load(f)
                 self.this_tweet_num = self.dict_features.pop("last_position")
-                print(self.dict_features)
                 
                 # Load feature whitelist
                 with open("feature_whitelist.json") as f:
@@ -28,6 +30,10 @@ class TweetDataEntryTool(QWidget):
                 # Load tweets
                 with open("high_rt_tweets.json") as f:
                     self.tweets = json.load(f)
+                
+                # Setup reverse lookup for tweets
+                split_id_tweet = [re.split(":", self.tweets[str(i)], maxsplit=2) for i in range(len(self.tweets))]
+                self.tweet_id_lookup = {split_id_tweet[i][0]: str(i) for i in range(len(self.tweets))}
                 
                 # Set window
                 self.setWindowTitle("Trump Tweet Data Entry Tool")
@@ -89,72 +95,78 @@ class TweetDataEntryTool(QWidget):
                 self.layout_right.addWidget(self.enter_id)
                 self.enter_id.textChanged.connect(self.txtTweetId_Change)
 
-                
                 # Load features into cells
-                self.dict_keys = list(self.dict_features.keys())
-                self.this_key = self.dict_keys[self.this_tweet_num]
-                features_and_references = self.dict_features[self.this_key]
-                list_keys = list(features_and_references.keys())
-                
-                for i in range(9):
+                for i in range(NUM_WINDOWS):
                     exec("self.textEdit{i} = QTextEdit()".format(i=i))
                     exec("self.layout_left.addWidget(self.textEdit{i})".format(i=i))
                     exec("self.textEdit{i}.resize(100, 250)".format(i=i))
-                    
-                    if i < len(features_and_references):
-                        this_reference_list = features_and_references[list_keys[i]]
-                        reference_list_str = ", ".join(this_reference_list)
-                        set_text = "{feature}\n".format(feature=list_keys[i])
-                        set_text += "{reference_list}".format(reference_list=reference_list_str)
-                        exec("self.textEdit{i}.setPlainText(set_text)".format(i=i))
-                        
+                self.populate_features()
+                
                 self.setLayout(self.layout_main)
       
+        # Reload features and references into the text edit boxes
+        def populate_features(self):
+            
+            features_and_references = self.dict_features[self.this_id] 
+            list_keys = list(features_and_references.keys())
+            
+            for i in range(NUM_WINDOWS):
+                if i < len(features_and_references):
+                    this_reference_list = features_and_references[list_keys[i]]
+                    reference_list_str = ", ".join(this_reference_list)
+                    set_text = "{feature}\n".format(feature=list_keys[i])
+                    set_text += "{reference_list}".format(reference_list=reference_list_str)
+                    exec("self.textEdit{i}.setPlainText(set_text)".format(i=i))
+                else:
+                    set_text = ""
+                    exec("self.textEdit{i}.setPlainText(set_text)".format(i=i))
+                    
+            split_id_tweet = re.split(":", self.tweets[self.tweet_id_lookup[self.this_id]], maxsplit=1)
+            self.this_tweet.setText(split_id_tweet[1])
+            self.enter_id.setText(self.this_id)
+            self.tweet_num_enter.setText(self.tweet_id_lookup[self.this_id])
+
+            
         # Set Tweet num
         def set_tweet_num(self, tweet_num):
-            split_id_tweet = re.split(":", self.tweets[tweet_num], maxsplit=1)
-            self.this_id = split_id_tweet[0]
-            self.enter_id.setText(self.this_id)
-            self.this_tweet.setText(split_id_tweet[1])
-        
+            self.this_tweet_num = tweet_num
+            self.this_id = re.split(":", self.tweets[str(self.this_tweet_num)], maxsplit=1)[0]
+            self.populate_features()
+
+            
         # Set Tweet ID
         def set_tweet_id(self, tweet_id):
-            
-            # Cycle through tweets to find corresponding ID
-            for i in range(len(self.tweets)):
-                split_id_tweet = re.split(":", self.tweets[i], maxsplit=1)
-                if int(split_id_tweet[0]) == tweet_id:
-                    self.this_tweet.setText(split_id_tweet[1])
-                    self.tweet_num_enter.setText(str(i))
-
-            self.this_id = tweet_id
-            
+            self.this_id = tweet_id.replace("\n", "")
+            self.this_tweet_num = int(self.tweet_id_lookup[self.this_id])
+            self.populate_features()
+        
         # Advance to next tweet by decreasing RT
         def btnPressFwd_Clicked(self):
-                self.this_tweet_num = int(self.tweet_num_enter) + 1
-                self.tweet_num_enter.setText(str(self.this_tweet_num))
-                self.set_tweet_num(self.this_tweet_num)
+                self.set_tweet_num(int(self.tweet_num_enter.toPlainText()) + 1)
 
         # Go back to previous tweet
         def btnPressBack_Clicked(self):
-                self.this_tweet_num = int(self.tweet_num_enter) - 1
-                self.tweet_num_enter.setText(str(self.this_tweet_num))
-                self.set_tweet_num(self.this_tweet_num)
-        
+                self.set_tweet_num(int(self.tweet_num_enter.toPlainText()) - 1)   
+                
         # Select tweet by tweet ID
         def txtTweetId_Change(self):
-            self.this_id = self.enter_id.getText()
-            self.set_tweet_id(self.this_tweet_id)
-        
+            if len(inspect.stack()) > 3:
+                return
+            
+            if inspect.stack()[0][3] in ["txtTweetId_Change"]:
+                self.set_tweet_id(self.enter_id.toPlainText())
+                            
         # Select tweet by RT order
         def txtTweetNum_Change(self):
-            self.this_tweet_num = int(self.enter_id.getText())
-            self.set_tweet_num(self.this_tweet_num)
-        
-        # Save the file once a change is made to the feature or reference URL
-        def saveAll(self):
-            with open("more_features.json", 'rw') as f:
-                json.dump(self.dict_features)
+            if len(inspect.stack()) > 3:
+                return
+            
+            if inspect.stack()[0][3] in ["txtTweetNum_Change"]:
+                self.set_tweet_num(int(self.tweet_num_enter.toPlainText()))
+                        
+        # Update self.dict_features and save the file once a change is made to the feature or reference URL
+        def onFeatureChange(self):
+            pass
 
 if __name__ == '__main__':
         app = QApplication(sys.argv)
