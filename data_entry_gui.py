8af206c7030f75c9ea388fe2f1f5b74135529bea
Matthew <mattwoods243@outlook.com>
2022-06-10 18:08:37 -1000

    resolved issue of knowing whether a tweet is unreviewed or not by inserting none by default. tweets which are reviewd with no tags are simply empty.

--- data_entry_gui.py
+++ data_entry_gui.py
@@ -41,7 +41,7 @@ class TweetDataEntryTool(QWidget):
                 list_keys = list(self.dict_features.keys())
                 for this_id in self.tweet_df["id"].values:
                     if str(this_id) not in list_keys:
-                        self.dict_features[str(this_id)] = {}
+                        self.dict_features[str(this_id)] = {"none": []}
                     
                 # Set window
                 self.setWindowTitle("Trump Tweet Data Entry Tool")
@@ -80,20 +80,25 @@ class TweetDataEntryTool(QWidget):
                 self.layout_left.addWidget(self.this_tweet)
                 
                 # Add whitelist features 
+                self.whitelist_features_title = QLabel()
+                self.whitelist_features_title.setText("Whitelist Features: ")
+                self.whitelist_features_title.setFont(self.this_font)
                 self.whitelist_features_label = QTextEdit()
-                self.whitelist_features_label.setText("Whitelist Features: " + ", ".join(self.feature_whitelist))
+                self.whitelist_features_label.setText(", ".join(self.feature_whitelist))
                 self.whitelist_features_label.setFont(self.this_font)
                 self.whitelist_features_label.setFixedSize(500, 500)
                 self.whitelist_features_label.setReadOnly(True)
+                self.layout_right.addWidget(self.whitelist_features_title)
                 self.layout_right.addWidget(self.whitelist_features_label)
                 
-                #tAdd tweet number from top RT tweets (0 = highest RTs)
+                # Add tweet number from top RT tweets (0 = highest RTs)
                 self.tweet_num = QLabel()
                 self.tweet_num.setText("\nTweet Number From Top By RT:")
                 self.tweet_num.setFont(self.this_font)
                 self.layout_right.addWidget(self.tweet_num)
                 self.tweet_num_enter = QTextEdit()
                 self.tweet_num_enter.setText(str(self.this_tweet_num))
+                self.tweet_num_enter.setFont(self.this_font)
                 self.layout_right.addWidget(self.tweet_num_enter)
                 self.tweet_num_enter.textChanged.connect(self.txtTweetNum_Change)
                 
@@ -104,6 +109,7 @@ class TweetDataEntryTool(QWidget):
                 self.layout_right.addWidget(self.tweet_id)
                 self.enter_id = QTextEdit()
                 self.enter_id.setText(self.this_id)
+                self.enter_id.setFont(self.this_font)
                 self.layout_right.addWidget(self.enter_id)
                 self.enter_id.textChanged.connect(self.txtTweetId_Change)
 
@@ -111,12 +117,13 @@ class TweetDataEntryTool(QWidget):
                 for i in range(NUM_WINDOWS):
                     exec("self.textEdit{i} = QTextEdit()".format(i=i))
                     exec("self.textEdit{i}.textChanged.connect(self.onFeatureChange)".format(i=i))
+                    exec("self.textEdit{i}.cursorPositionChanged.connect(self.onCursorChange)".format(i=i))
                     exec("self.textEdit{i}.setObjectName('textEdit{i}')".format(i=i))
-                    exec("self.textEdit{i}.last_feature = ''".format(i=i))
+                    exec("self.textEdit{i}.setFont(self.this_font)".format(i=i))
                     exec("self.layout_left.addWidget(self.textEdit{i})".format(i=i))
                     exec("self.textEdit{i}.resize(100, 250)".format(i=i))
                 self.populate_features()
-                
+                self.onCursorChange()
                 self.setLayout(self.layout_main)
       
         # Reload features and references into the text edit boxes
@@ -136,6 +143,7 @@ class TweetDataEntryTool(QWidget):
                 else:
                     set_text = ""
                     exec("self.textEdit{i}.setPlainText(set_text)".format(i=i))
+                    
             tweet_str = str(self.tweet_df.loc[[self.this_tweet_num], ["text"]].values[0, 0]) + " "
             tweet_str += str(self.tweet_df.loc[[self.this_tweet_num], ["date"]].values[0, 0])[0:10]
             self.this_tweet.setText(tweet_str)
@@ -184,6 +192,22 @@ class TweetDataEntryTool(QWidget):
             
             if inspect.stack()[0][3] in ["txtTweetNum_Change"]:
                 self.set_tweet_num(int(self.tweet_num_enter.toPlainText()))
+                # Save last feature selected, if any.
+        
+        # Keep track of previous feature
+        def onCursorChange(self):
+            if len(inspect.stack()) > 3:
+                            return
+            
+            if self.focusWidget():
+                object_name = self.focusWidget().objectName()
+                if inspect.stack()[0][3] in ["onCursorChange"] and object_name:
+                    exec("self.this_list = self.{obname}.toPlainText().split({char})".format(char="\n", obname=object_name))
+
+                    if self.this_list:
+                        this_feature = self.this_list[0]
+                        if (this_feature in self.feature_whitelist) or (this_feature == "none"):
+                            exec("self.{obname}.last_feature = this_feature".format(obname=object_name))
                         
         # Update self.dict_features once a change is made to the feature or reference URL
         # TODO: data_entry_gui_test.test_onFeatureChange(self)
@@ -225,16 +249,20 @@ class TweetDataEntryTool(QWidget):
                             p(self.dict_features[self.this_id][this_feature])
                             p(this_feature)
                             p("onFeatureChange: references updated in dict_features")
-                
-                        exec("self.{obname}.last_feature = this_feature".format(obname=object_name))
+                            
+                        if this_feature:
+                            exec("self.{obname}.last_feature = this_feature".format(obname=object_name))
 
 
                 # Delete the feature from the file if it is erased in the gui                    
                 else:
                     try:
                         exec("del self.dict_features[self.this_id][self.{obname}.last_feature]".format(obname=object_name))
-                    except:
-                        pass
+                        
+                    except Exception as e:
+                        p(e)
+                        p("dict_features_not_updated")
+            
            
                               
 if __name__ == '__main__':
